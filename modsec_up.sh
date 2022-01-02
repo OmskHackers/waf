cbbd8d79abac542c12ff06256649d0c5245b3502
Ilya Starchenko <redux1234567@mail.ru>
2022-01-02 18:43:43 +0600

    add ws

--- modsec_up.sh
+++ modsec_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "usage: modsec_up.sh [port] [address] [absolute path to config folder]"
+    echo "usage: modsec_up.sh [port] [address] [absolute path to config folder] [address to WS(optionally)]"
     exit
 fi
 
-docker run -p 80:$1 -ti -e EXECUTING_PARANOIA=10 -e ANOMALY_INBOUND=10 -e MODSEC_REQ_BODY_ACCESS=on -e ANOMALY_OUTBOUND=5 -e PROXY=1 -e BACKEND=$2 -v $3:/opt/owasp-crs/rules:ro --rm owasp/modsecurity-crs:nginx-alpine
+docker run -p 80:$1 -ti -e BACKEND_WS=ws://localhost:8080 -e EXECUTING_PARANOIA=10 -e ANOMALY_INBOUND=10 -e MODSEC_REQ_BODY_ACCESS=on -e ANOMALY_OUTBOUND=5 -e PROXY=1 -e BACKEND=$2 -v $3:/opt/owasp-crs/rules:ro --rm owasp/modsecurity-crs:nginx-alpine
