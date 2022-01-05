69f278f74bd74e58251edc8d46e76bf662d643e4
lucky624 <lironnanda@gmail.com>
2022-01-06 03:41:27 +0600

    add background work -d

--- modsec_up.sh
+++ modsec_up.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 3 ]; then
     exit
 fi
 
-docker run -p $1:80 -ti \
+docker run -p $1:80 -dti \
 -e BACKEND_WS=$4 -e EXECUTING_PARANOIA=10 -e ANOMALY_INBOUND=10 -e MODSEC_REQ_BODY_ACCESS=on -e ANOMALY_OUTBOUND=5 -e PROXY=1 -e BACKEND=$2 -e MODSEC_RULE_ENGINE=on \
 -v $3:/opt/owasp-crs/rules:ro \
 --rm owasp/modsecurity-crs:nginx-alpine
