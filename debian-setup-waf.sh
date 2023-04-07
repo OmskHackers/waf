5f912d8aa045e1c96d669c58b91b5efb8033e92d
lucky624 <lironnanda@gmail.com>
2023-04-07 17:15:11 +0600

    fix debian installer

--- debian-setup-waf.sh
+++ debian-setup-waf.sh
@@ -90,9 +90,15 @@ printInfo
 
 
 #Latest stable NGINX (important)
-echo -e "${c}Adding Latest Stable NGINX PPA from launchpad.net"; $r
-apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-sudo add-apt-repository ppa:nginx/stable -y
+apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor |  tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/debian `lsb_release -cs` nginx" \
+    | tee /etc/apt/sources.list.d/nginx.list
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | tee /etc/apt/preferences.d/99nginx
+
 apt-get update -y
 apt-get install -y nginx
 apt-get upgrade -y
